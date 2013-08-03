d0218ba569ff703ffbe4cf3ea172fba9000d0e8f
mwegel <mwegel@uni-potsdam.de>
2013-08-03 17:43:56 +0200

    Einlese-Funktionen

--- Main.py
+++ Main.py
@@ -1,12 +1,38 @@
 #/usr/bin/ python
 # -*- coding: utf8 -*-
+import csv               # Lesen und Schreiben von CSV-Dateien
 from numpy import array  # Datenstruktur für Vektoren
-import wv0               # Dictionary der initialen Wortvektoren {word : vector}
-import tweets            # Dictionary von Tweets {id : text}
-import stopwords         # Liste von Stoppwörtern
 
-def calc_next_generation(wv0):
-    wv1 = {}
+###########################################################
+# Nutzereingabe:
+# init_wv_file    CSV-Datei mit initialen Wortvektoren
+# tweets_file     Datei mit Tweets (JSON?)
+# stopwords_file  Textdatei mit Stoppwörtern
+###########################################################
+
+# CSV einlesen und als Dictionary von Wortvektoren ausgeben
+def csv_to_vectors(filename):
+    wv = {}
+    with open(filename,'r') as csvfile:
+        csvreader = csv.reader(csvfile,delimiter=';')
+        for stringlist in csvreader:
+            word = stringlist[0]
+            floatlist = [float(x) for x in stringlist[1:]]
+            wv[word] = array(floatlist)
+    return wv
+
+# Tweets einlesen und als Dictionary ID -> Text ausgeben
+def jsons_to_dict(filename):
+    ## ...
+
+# Raw Text einlesen und als Liste ausgeben
+def textfile_to_list(filename):
+    textfile = open(filename,'r')
+    return textfile.readlines()
+
+# Hauptalgorithmus: aus gegebener Generation von Wortvektoren die nächste berechnen
+def calc_next_generation(wvm):
+    wvn = {}
     tv = {}
     
     for id, text in tweets.iteritems():
@@ -14,23 +40,29 @@ def calc_next_generation(wv0):
         tv[id] = array([0,0,0,0,0,0,0])
         for token in text:
             if not token in stopwords:
-                tv[id] = tv[id] + wv0[token]
+                tv[id] = tv[id] + wvm[token]
                 
         for token in text:
             if not token in stopwords:
-                # Initialisierung von wv1[token] fehlt!
-                wv1[token] = wv1[token] + tv[id]
+                ## Initialisierung von wv1[token] fehlt! ##
+                wvn[token] = wvn[token] + tv[id]
                 
-    for word in wv1:
-        wv1[word] = normalize(wv1[word])
+    for word in wvn:
+        wvn[word] = normalize(wvn[word])
 
-    return wv1
+    return wvn
 
+# Herunterrechnen eines Vektors, so dass er Summe 1 hat
 def normalize(vector):
-    # Werden uns die Rundungsfehler auffressen?
+    ## Werden uns die Rundungsfehler auffressen? ##
     return vector/vector.sum()
 
 
+# Tweets und Stoppwörter einlesen
+tweets = jsons_to_dict(tweets_file)
+stopwords = textfile_to_list(stopwords_file)
+# Wortvektoren der 0. Generation einlesen
+wv0 = csv_to_vectors(init_wv_file)
 # Erster Durchlauf, um 1. Generation dann mit 0. Generation vergleichen zu können
 wv1 = calc_next_generation(wv0)
 # Schleife - vergleiche stets die gerade berechnete Generation mit der vorigen
