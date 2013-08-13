2aa01580691ff6d74888a1d4f0596d702176ec32
Johannes Gontrum <gontrum@uni-potsdam.de>
2013-08-13 14:04:09 +0200

    Anpassung Main

--- Main.py
+++ Main.py
@@ -4,6 +4,7 @@ import csv               # Lesen und Schreiben von CSV-Dateien
 import sys
 import os
 import json
+import codecs
 from numpy import array  # Datenstruktur für Vektoren
 from happyfuntokenizing import Tokenizer
 
@@ -14,21 +15,23 @@ from happyfuntokenizing import Tokenizer
 # stopwords_file  Textdatei mit Stoppwörtern
 ###########################################################
 init_wv_file = "Words/Regionalwords.csv"
-tweets_file = ""
-stopwords_file = ""
+tweets_file = "./"
+stopwords_file = "Wordcount/stpwds_no_happytok.txt"
 
 # CSV einlesen und als Dictionary von Wortvektoren ausgeben
 def csv_to_vectors(filename):
     wv = {}
-    with open(filename,'r') as csvfile:
-        csvreader = csv.reader(csvfile,delimiter=';')
+    with codecs.open(filename,'r',"utf8") as csvfile:
         # Erste Zeile ignorieren
-        for stringlist in csvreader[1:]:
+        for line in csvfile:
+            stringlist = line.split(";")
             word = stringlist[0]
             floatlist = list()
-            for x in stringlist[1:]:
+            for x in stringlist[1:8]:
                 if x == "0.33":
                     floatlist.append(float(1/3.0))
+                elif x == "":
+                    floatlist.append(0.0)
                 else:
                     floatlist.append(float(x))
             wv[word] = array(floatlist)
@@ -64,14 +67,16 @@ def calc_next_generation(wvm, tweets):
     tv = {}     # tv = Dict von TweetIDs auf Tweet-Vektoren
     
     for id, text in tweets.iteritems():
-        tv[id] = array([0,0,0,0,0,0,0])
+        tv[id] = array([0.0,0.0,0.0,0.0,0.0,0.0,0.0])
         for token in text:
-            tv[id] += wvm[token]
-        for token in text:
-            if token in wvn:
-                wvn[token] += tv[id]
-            else:
-                wvn[token] = tv[id]
+            if token in wvm:
+                tv[id] += wvm[token]
+        if tv[id].sum() > 0:
+            for token in text:
+                if token in wvn:
+                    wvn[token] += tv[id]
+                else:
+                    wvn[token] = tv[id]
 
     for word in wvn:
         wvn[word] = normalize(wvn[word])
@@ -81,8 +86,9 @@ def calc_next_generation(wvm, tweets):
 # Herunterrechnen eines Vektors, so dass er Summe 1 hat
 def normalize(vector):
     ## Werden uns die Rundungsfehler auffressen? ##
-    return vector/vector.sum()
-
+    if vector.sum() > 0:
+        return vector/vector.sum()
+    return array([0.0,0.0,0.0,0.0,0.0,0.0,0.0])
 
 # Tweets und Stoppwörter einlesen
 stopwords = textfile_to_list(stopwords_file)
@@ -93,14 +99,18 @@ wv0 = csv_to_vectors(init_wv_file)
 wv1 = calc_next_generation(wv0, tweets)
 # Schleife - vergleiche stets die gerade berechnete Generation mit der vorigen
 # Wenn sie größer ist, hat sie mehr Einträge, es sind also neue Wörter hinzugekommen
-while wv1 > wv0:
+c = 0
+while len(wv1) > len(wv0):
+    c += 1
     # In diesem Fall wird die alte Generation vergessen, ihren Platz nimmt die
     # gerade berechnete ein
     wv0 = wv1
     # Aus ihr wird die nächste Generation berechnet
-    wv1 = calc_next_generation(wv0)
+    wv1 = calc_next_generation(wv0, tweets)
 else:
     # Ist die gerade berechnete Generation nicht größer, enthielt die vorige bereits
     # alle Wörter des Trainingskorpus. Es wird also die vorige abgespeichert
-    save(wv0)
+    print wv0
+    print "fertig!!"
+    print c
 
