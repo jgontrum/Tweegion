bde405158de39c34e58a734eee87f325fd45d411
Johannes Gontrum <gontrum@uni-potsdam.de>
2013-08-19 21:38:31 +0200

    Example angepass (mal wieder)

--- example.py
+++ example.py
@@ -1,114 +1,60 @@
 #/usr/bin/ python
 # -*- coding: utf8 -*-
 from include.tweegion import Tweegion
-
-tweegion = Tweegion("geo",
-					"data/geo-tweets/balanced-39k.json",
-					"data/stpwds_no_happytok.txt",
-					1,
-					"data/geo-tweets/balanced-21k.json",
-					"")
-tweegion.evaluate_accuracy("data/geo-tweets/gold.json")
-
-tweegion = Tweegion("geo",
-					"data/geo-tweets/balanced-39k.json",
-					"data/stpwds_no_happytok.txt",
-					2,
-					"data/geo-tweets/balanced-21k.json",
-					"")
-tweegion.evaluate_accuracy("data/geo-tweets/gold.json")
-
-
-tweegion = Tweegion("geo",
-					"data/geo-tweets/balanced-39k.json",
-					"data/stpwds_no_happytok.txt",
-					5,
-					"data/geo-tweets/balanced-21k.json",
-					"")
-tweegion.evaluate_accuracy("data/geo-tweets/gold.json")
-
-
-tweegion = Tweegion("geo",
-					"data/geo-tweets/balanced-39k.json",
-					"data/stpwds_no_happytok.txt",
-					10,
-					"data/geo-tweets/balanced-21k.json",
-					"")
-tweegion.evaluate_accuracy("data/geo-tweets/gold.json")
+from include.tweegion_log import Tweegion as TweegionLog
 
 # ~~~~~~~~~~~~~~
-
 tweegion = Tweegion("geo",
+					"data/geo-tweets/balanced-61k.json",
+					"data/stoppwords200.txt",
+					0,
 					"data/geo-tweets/balanced-39k.json",
-					"data/stoppwords1000.txt",
-					1,
-					"data/geo-tweets/balanced-21k.json",
 					"")
 tweegion.evaluate_accuracy("data/geo-tweets/gold.json")
 
-
 tweegion = Tweegion("geo",
-					"data/geo-tweets/balanced-39k.json",
-					"data/stoppwords1000.txt",
-					5,
-					"data/geo-tweets/balanced-21k.json",
-					"")
-tweegion.evaluate_accuracy("data/geo-tweets/gold.json")
-
-# ~~~~~~~~~~~~~~
-
-tweegion = Tweegion("geo",
-					"data/geo-tweets/balanced-39k.json",
-					"data/stoppwords500.txt",
-					1,
-					"data/geo-tweets/balanced-21k.json",
-					"")
-tweegion.evaluate_accuracy("data/geo-tweets/gold.json")
-
-
-tweegion = Tweegion("geo",
-					"data/geo-tweets/balanced-39k.json",
-					"data/stoppwords500.txt",
-					5,
-					"data/geo-tweets/balanced-21k.json",
-					"")
-tweegion.evaluate_accuracy("data/geo-tweets/gold.json")
-
-# ~~~~~~~~~~~~~~
-
-tweegion = Tweegion("geo",
-					"data/geo-tweets/balanced-39k.json",
-					"data/stoppwords750.txt",
-					1,
-					"data/geo-tweets/balanced-21k.json",
-					"")
-tweegion.evaluate_accuracy("data/geo-tweets/gold.json")
-
-
-tweegion = Tweegion("geo",
-					"data/geo-tweets/balanced-39k.json",
-					"data/stoppwords750.txt",
-					5,
-					"data/geo-tweets/balanced-21k.json",
-					"")
-tweegion.evaluate_accuracy("data/geo-tweets/gold.json")
-
-# ~~~~~~~~~~~~~~
-
-tweegion = Tweegion("geo",
-					"data/geo-tweets/balanced-39k.json",
+					"data/geo-tweets/balanced-61k.json",
 					"data/stoppwords200.txt",
 					1,
-					"data/geo-tweets/balanced-21k.json",
+					"data/geo-tweets/balanced-39k.json",
 					"")
 tweegion.evaluate_accuracy("data/geo-tweets/gold.json")
 
 
 tweegion = Tweegion("geo",
-					"data/geo-tweets/balanced-39k.json",
+					"data/geo-tweets/balanced-61k.json",
 					"data/stoppwords200.txt",
-					5,
-					"data/geo-tweets/balanced-21k.json",
+					3,
+					"data/geo-tweets/balanced-39k.json",
 					"")
 tweegion.evaluate_accuracy("data/geo-tweets/gold.json")
+
+# ~~~~~~~~~~~~~~
+tweegion = TweegionLog("geo",
+					"data/geo-tweets/balanced-61k.json",
+					"data/stoppwords200.txt",
+					0,
+					"data/geo-tweets/balanced-39k.json",
+					"")
+tweegion.evaluate_accuracy("data/geo-tweets/gold.json")
+
+tweegion = TweegionLog("geo",
+					"data/geo-tweets/balanced-61k.json",
+					"data/stoppwords200.txt",
+					1,
+					"data/geo-tweets/balanced-39k.json",
+					"")
+tweegion.evaluate_accuracy("data/geo-tweets/gold.json")
+
+
+tweegion = TweegionLog("geo",
+					"data/geo-tweets/balanced-61k.json",
+					"data/stoppwords200.txt",
+					3,
+					"data/geo-tweets/balanced-39k.json",
+					"")
+tweegion.evaluate_accuracy("data/geo-tweets/gold.json")
+
+
+
 
