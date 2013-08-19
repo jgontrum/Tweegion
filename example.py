153914a9b1a2f6e3d535c933a37416e45e15cc87
Johannes Gontrum <gontrum@uni-potsdam.de>
2013-08-19 19:00:50 +0200

    Erste Tests, werden nach results.txt geschrieben.

--- example.py
+++ example.py
@@ -1,6 +1,5 @@
 #/usr/bin/ python
 # -*- coding: utf8 -*-
-
 from include.tweegion import Tweegion
 
 tweegion = Tweegion("geo",
@@ -9,8 +8,107 @@ tweegion = Tweegion("geo",
 					1,
 					"data/geo-tweets/balanced-21k.json",
 					"")
-
-# print tweegion.classify("Dies ist ein ziemlich neutraler Beispieltweet.", False)
-# print tweegion.classify("Dies ist ein ziemlich neutraler Beispieltweet.", True)
-
 tweegion.evaluate_accuracy("data/geo-tweets/gold.json")
+
+tweegion = Tweegion("geo",
+					"data/geo-tweets/balanced-39k.json",
+					"data/stpwds_no_happytok.txt",
+					2,
+					"data/geo-tweets/balanced-21k.json",
+					"")
+tweegion.evaluate_accuracy("data/geo-tweets/gold.json")
+
+
+tweegion = Tweegion("geo",
+					"data/geo-tweets/balanced-39k.json",
+					"data/stpwds_no_happytok.txt",
+					5,
+					"data/geo-tweets/balanced-21k.json",
+					"")
+tweegion.evaluate_accuracy("data/geo-tweets/gold.json")
+
+
+tweegion = Tweegion("geo",
+					"data/geo-tweets/balanced-39k.json",
+					"data/stpwds_no_happytok.txt",
+					10,
+					"data/geo-tweets/balanced-21k.json",
+					"")
+tweegion.evaluate_accuracy("data/geo-tweets/gold.json")
+
+# ~~~~~~~~~~~~~~
+
+tweegion = Tweegion("geo",
+					"data/geo-tweets/balanced-39k.json",
+					"data/stoppwords1000.txt",
+					1,
+					"data/geo-tweets/balanced-21k.json",
+					"")
+tweegion.evaluate_accuracy("data/geo-tweets/gold.json")
+
+
+tweegion = Tweegion("geo",
+					"data/geo-tweets/balanced-39k.json",
+					"data/stoppwords1000.txt",
+					5,
+					"data/geo-tweets/balanced-21k.json",
+					"")
+tweegion.evaluate_accuracy("data/geo-tweets/gold.json")
+
+# ~~~~~~~~~~~~~~
+
+tweegion = Tweegion("geo",
+					"data/geo-tweets/balanced-39k.json",
+					"data/stoppwords500.txt",
+					1,
+					"data/geo-tweets/balanced-21k.json",
+					"")
+tweegion.evaluate_accuracy("data/geo-tweets/gold.json")
+
+
+tweegion = Tweegion("geo",
+					"data/geo-tweets/balanced-39k.json",
+					"data/stoppwords500.txt",
+					5,
+					"data/geo-tweets/balanced-21k.json",
+					"")
+tweegion.evaluate_accuracy("data/geo-tweets/gold.json")
+
+# ~~~~~~~~~~~~~~
+
+tweegion = Tweegion("geo",
+					"data/geo-tweets/balanced-39k.json",
+					"data/stoppwords750.txt",
+					1,
+					"data/geo-tweets/balanced-21k.json",
+					"")
+tweegion.evaluate_accuracy("data/geo-tweets/gold.json")
+
+
+tweegion = Tweegion("geo",
+					"data/geo-tweets/balanced-39k.json",
+					"data/stoppwords750.txt",
+					5,
+					"data/geo-tweets/balanced-21k.json",
+					"")
+tweegion.evaluate_accuracy("data/geo-tweets/gold.json")
+
+# ~~~~~~~~~~~~~~
+
+tweegion = Tweegion("geo",
+					"data/geo-tweets/balanced-39k.json",
+					"data/stoppwords200.txt",
+					1,
+					"data/geo-tweets/balanced-21k.json",
+					"")
+tweegion.evaluate_accuracy("data/geo-tweets/gold.json")
+
+
+tweegion = Tweegion("geo",
+					"data/geo-tweets/balanced-39k.json",
+					"data/stoppwords200.txt",
+					5,
+					"data/geo-tweets/balanced-21k.json",
+					"")
+tweegion.evaluate_accuracy("data/geo-tweets/gold.json")
+
