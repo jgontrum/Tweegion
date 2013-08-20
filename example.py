67b6856dae70777f67939b8006ea7e39aae4e172
Johannes Gontrum <gontrum@uni-potsdam.de>
2013-08-20 20:38:17 +0200

    Typo

--- example.py
+++ example.py
@@ -4,8 +4,8 @@ from include.tweegion import Tweegion
 from include.tweegion_root import Tweegion as TweegionRoot
 
 blackwords = ["data/stoppwords200.txt", "data/stoppwords500.txt", "data/stoppwords750.txt", "data/stoppwords1000.txt"]
-tweets = ["data/geo-tweets/balanced-21k.json", "data/geo-tweets/balanced-39k.json", "data/geo-tweets/balanced-61k.json", "data/geo-tweets/unbalanced-175k.json"]
-geo = ["data/geo-tweets/balanced-21k.json", "data/geo-tweets/balanced-39k.json", "data/geo-tweets/balanced-61k.json", "data/geo-tweets/unbalanced-175k.json"]
+tweets = ["data/geo-tweets/balanced-21k.json", "data/geo-tweets/balanced-39k.json", "data/geo-tweets/balanced-61k.json", "data/geo-tweets/unbalanced_175k.json"]
+geo = ["data/geo-tweets/balanced-21k.json", "data/geo-tweets/balanced-39k.json", "data/geo-tweets/balanced-61k.json", "data/geo-tweets/unbalanced_175k.json"]
 loops = [0,1,3,5,10]
 
 for loop in loops:
