0c31e709a7a30743b8f2b2f858f5fa08b5fbf629
Johannes Gontrum <gontrum@uni-potsdam.de>
2013-08-21 23:35:42 +0200

    Vegl. Root vs  Legacy

--- example_single.py
+++ example_single.py
@@ -13,3 +13,31 @@ tweegion = Tweegion("geo",
 tweegion.evaluate_accuracy("data/geo-tweets/gold.json")
 
 
+tweegion = TweegionRoot("geo",
+ 					"data/geo-tweets/balanced-39k.json",
+ 					"data/stoppwords200.txt",
+ 					1,
+ 					"data/geo-tweets/balanced-61k.json",
+ 					"data/regionalwords.csv",
+ 					0.33 )
+tweegion.evaluate_accuracy("data/geo-tweets/gold.json")
+
+tweegion = Tweegion("geo",
+ 					"data/geo-tweets/balanced-39k.json",
+ 					"data/stoppwords200.txt",
+ 					0,
+ 					"data/geo-tweets/balanced-61k.json",
+ 					"data/regionalwords.csv",
+ 					0.33 )
+tweegion.evaluate_accuracy("data/geo-tweets/gold.json")
+
+
+tweegion = TweegionRoot("geo",
+ 					"data/geo-tweets/balanced-39k.json",
+ 					"data/stoppwords200.txt",
+ 					0,
+ 					"data/geo-tweets/balanced-61k.json",
+ 					"data/regionalwords.csv",
+ 					0.33 )
+tweegion.evaluate_accuracy("data/geo-tweets/gold.json")
+
