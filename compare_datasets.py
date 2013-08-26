1bbabea5cdf3d26d2e540a2a59f95de6029bf35c
Johannes Gontrum <gontrum@uni-potsdam.de>
2013-08-26 23:30:18 +0200

    Script to compare all dataset combinations

---
+++ compare_datasets.py
@@ -0,0 +1,22 @@
+#/usr/bin/ python
+# -*- coding: utf8 -*-
+from include.tweegion import Tweegion
+from include.tweegion_root import Tweegion as TweegionRoot
+from include.tweegion_log import Tweegion as TweegionLog
+
+blackwords = ["data/stoppwords200.txt"]
+tweets = ["data/geo-tweets/balanced-21k.json", "data/geo-tweets/balanced-39k.json", "data/geo-tweets/balanced-61k.json", "data/geo-tweets/unbalanced_175k.json", "data/corpus/2days_1500k.json"]
+geo = ["data/geo-tweets/balanced-21k.json", "data/geo-tweets/balanced-39k.json", "data/geo-tweets/balanced-61k.json", "data/geo-tweets/unbalanced_175k.json"]
+loops = [1]
+
+for loop in loops:
+	for blackword in blackwords:
+		for tweet in tweets:
+			for geot in geo:
+				tweegion = Tweegion("geo",
+ 					tweet,
+ 					blackword,
+ 					loop,
+ 					geot,
+ 					"")
+				tweegion.evaluate_accuracy("data/geo-tweets/gold.json")
