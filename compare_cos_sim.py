12b71feb65a389a79c6e7483778317547b05c71f
Johannes Gontrum <johannes.gontrum@uni-potsdam.de>
2013-08-28 10:19:52 +0200

    Neue Ergebnisse Cos vergleich

--- compare_cos_sim.py
+++ compare_cos_sim.py
@@ -4,8 +4,8 @@ from include.tweegion import Tweegion
 
 blackwords = ["data/stoppwords200.txt"]
 tweets = ["data/geo-tweets/unbalanced_175k.json"]
-geo = ["data/geo-tweets/balanced-39k.json"]
-loops = [1]
+geo = ["data/geo-tweets/balanced-61k.json"]
+loops = [0]
 guesses = [0.9999999, 0.9, 0.8, 0.7, 0.6, 0.5, 0.4, 0.3, 0.2, 0.1, 0.05]
 
 for guess in guesses:
